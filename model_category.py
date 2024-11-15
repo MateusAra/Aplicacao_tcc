1709478d17ebc16d2f7a76bcc0ca379a72f7f5e7
MateusAra <mateus19araujo@hotmail.com>
2024-11-15 10:15:37 -0300

    melhorando acuracia do modelo

--- model_category.py
+++ model_category.py
@@ -8,9 +8,9 @@ from collections import defaultdict
 import plotly.express as px
 
 
-final_good = ['anon3', 'anon5', 'anon6', 'anon7', 'anon12', 'anon13', 'anon15', 'anon18', 'anon26', 'anon27', 'anon28', 'anon29', 'anon30', 'anon31', 'anon33', 'anon34', 'anon35', 'anon36', 'anon39', 'anon40', 'anon41', 'anon42', 'anon43', 'anon44', 'anon46', 'anon47', 'anon48', 'anon49', 'anon51', 'anon52', 'anon53', 'anon54', 'anon55', 'anon57', 'anon58', 'anon59', 'anon60', 'anon61', 'anon62', 'anon63', 'anon64', 'anon65', 'anon66', 'anon67', 'anon68', 'anon69', 'anon70', 'anon71', 'anon72', 'anon73', 'anon74', 'anon75', 'anon76', 'anon77', 'anon78', 'anon82', 'anon83', 'anon84', 'anon85', 'anon86', 'anon87', 'anon89', 'anon91', 'anon92', 'anon93', 'anon94', 'anon96', 'anon98']
-final_regular = ['anon4', 'anon9', 'anon10', 'anon14', 'anon16', 'anon17', 'anon19', 'anon20', 'anon21', 'anon22', 'anon23', 'anon25', 'anon45', 'anon50', 'anon56', 'anon79', 'anon80', 'anon88', 'anon90', 'anon97']
-final_critical = ['anon2', 'anon8', 'anon11', 'anon24', 'anon81', 'anon95', 'anon99', 'anon100']
+final_good = ['anon3', 'anon5', 'anon6', 'anon7', 'anon12', 'anon15', 'anon18', 'anon26', 'anon27', 'anon28', 'anon29', 'anon30', 'anon31', 'anon33', 'anon34', 'anon35', 'anon39', 'anon40', 'anon41', 'anon42', 'anon43', 'anon44', 'anon46', 'anon47', 'anon48', 'anon49', 'anon51', 'anon52', 'anon53', 'anon54', 'anon55', 'anon57', 'anon58', 'anon59', 'anon60', 'anon61', 'anon62', 'anon63', 'anon64', 'anon65', 'anon66', 'anon68', 'anon69', 'anon70', 'anon72', 'anon73', 'anon74', 'anon75', 'anon76', 'anon78', 'anon82', 'anon83', 'anon84', 'anon85', 'anon91', 'anon92', 'anon93', 'anon94', 'anon96', 'anon98']
+final_regular = ['anon4', 'anon9', 'anon10', 'anon14', 'anon16', 'anon17', 'anon19', 'anon20', 'anon21', 'anon22', 'anon23', 'anon25', 'anon45', 'anon50', 'anon80', 'anon88', 'anon90', 'anon97']
+final_critical = ['anon8', 'anon11', 'anon24', 'anon81', 'anon95', 'anon99', 'anon100']
 
 def get_clusters(clustering: bool = False):
 
@@ -39,7 +39,13 @@ def get_clusters(clustering: bool = False):
         data_frame = data_frame.dropna()
         clusters = data_frame.values.tolist()
 
-        return clusters
+        data = []
+
+        for line in clusters:
+            if line[9] <= 20:
+                data.append(line)
+        
+        return data
     
 def group_attempts_by_student(clusters):
     student_attempts = defaultdict(list)
@@ -54,29 +60,60 @@ def calculate_student_averages(student_attempts):
     student_averages = {}
 
     for name, attempts in student_attempts.items():
-        total_grade = 0
-        total_time = 0
-        total_attempts = 0
-        
+        # Organizar tentativas por atividade
+        attempts_by_activity = {}
         for attempt in attempts:
-            grade = float(attempt[4])
-            time = int(attempt[10])
-            attempts_realized = int(attempt[1])
-            
-            total_grade += grade
-            total_time += time
-            total_attempts += attempts_realized
+            activity = attempt[0]  # Índice da atividade
+            if activity not in attempts_by_activity:
+                attempts_by_activity[activity] = []
+            attempts_by_activity[activity].append(attempt)
 
-        num_attempts = len(attempts)
-        average_grade = total_grade / num_attempts
-        average_time = total_time / num_attempts
-        average_attempts = total_attempts / num_attempts
+        activity_averages = {}
+
+        total_grade_all_activities = 0
+        total_time_all_activities = 0
+        total_attempts_all_activities = 0
+        total_activities = 0
+
+        for activity, activity_attempts in attempts_by_activity.items():
+            sorted_attempts = sorted(activity_attempts, key=lambda x: float(x[4]), reverse=True)
+            
+            best_attempts = sorted_attempts[:4]
+
+            # Calcular as médias com base nas melhores tentativas
+            total_grade = sum(float(attempt[4]) for attempt in best_attempts)
+            total_time = sum(int(attempt[10]) for attempt in best_attempts)
+            total_attempts = sum(int(attempt[1]) for attempt in best_attempts)
+
+            num_best_attempts = len(best_attempts)
+            average_grade = total_grade / num_best_attempts
+            average_time = total_time / num_best_attempts
+            average_attempts = total_attempts / num_best_attempts
+
+            total_grade_all_activities += average_grade
+            total_time_all_activities += average_time
+            total_attempts_all_activities += average_attempts
+            total_activities += 1
+
+            activity_averages[activity] = {
+                'average_grade': average_grade,
+                'average_time': average_time,
+                'average_attempts': average_attempts,
+                'best_attempts': best_attempts
+            }
+
+        if total_activities > 0:
+            general_average_grade = total_grade_all_activities / total_activities
+            general_average_time = total_time_all_activities / total_activities
+            general_average_attempts = total_attempts_all_activities / total_activities
+        else:
+            general_average_grade = general_average_time = general_average_attempts = 0
 
         student_averages[name] = {
-            'average_grade': average_grade,
-            'average_time': average_time,
-            'average_attempts': average_attempts,
-            'attempts': attempts 
+            'activities': activity_averages,
+            'average_grade': general_average_grade,
+            'average_time': general_average_time,
+            'average_attempts': general_average_attempts
         }
 
     return student_averages
@@ -86,6 +123,7 @@ def categorize_students(student_averages):
     regular = []
     alert = []
     critical = []
+    list_of_recommended = []
     
     profile_good = Profile.get_profile_good()
     profile_regular = Profile.get_profile_regular()
@@ -96,14 +134,17 @@ def categorize_students(student_averages):
         avg_time = averages['average_time']
         avg_attempts = averages['average_attempts']
 
+        if avg_grade < 1 or avg_attempts > 4 or avg_time > 1200:
+            continue
+
         if avg_grade >= profile_good.media_grade \
         and avg_time <= profile_good.media_time \
         and avg_attempts <= profile_good.media_attempts:
             good.append([name, avg_grade, avg_time, avg_attempts, "Good"])
 
         elif profile_critical.media_grade <= avg_grade <= profile_good.media_grade \
-        and avg_time <= profile_regular.media_time \
-        and avg_attempts <= profile_regular.media_attempts:
+        and profile_good.media_time <= avg_time <= profile_critical.media_time \
+        and profile_good.media_attempts <= avg_attempts <= profile_critical.media_attempts:
             regular.append([name, avg_grade, avg_time, avg_attempts, "Regular"])
 
         elif avg_grade <= profile_critical.media_grade \
@@ -112,7 +153,7 @@ def categorize_students(student_averages):
             critical.append([name, avg_grade, avg_time, avg_attempts, "Critical"])
 
         else:
-            if (profile_good.media_grade >= avg_grade >= profile_critical.media_grade):
+            if (avg_grade <= profile_good.media_grade and avg_grade >= profile_critical.media_grade):
                 regular.append([name, avg_grade, avg_time, avg_attempts, "Regular"])
             elif (avg_grade >= profile_good.media_grade):
                 good.append([name, avg_grade, avg_time, avg_attempts, "Good"])
@@ -120,33 +161,19 @@ def categorize_students(student_averages):
                 critical.append([name, avg_grade, avg_time, avg_attempts, "Critical"])
 
             alert.append([name, avg_grade, avg_time, avg_attempts, "Alert"])
+        
+        # attempts = averages['attempts']
 
-    return good, regular, alert, critical
+        # for line in attempts:
+        #     if float(line[4]) < 7000.00:
+        #         list_of_recommended.append([name, line[12], line[1], line[4], line[9], line[8], line[3], line[6]])
+
+    return good, regular, alert, critical, list_of_recommended
 
 def verify_student_in_list(student_name: str, student_list: list) -> bool:
     student_name = student_name.strip().lower()
 
     return any(student_name == student.strip().lower() for student in student_list)
-
-def verify_student_is_outlier(username: str, student_list: list):
-    username = username.strip().lower()
-    for line in student_list:
-        if line[0].strip().lower() == username:
-            return True
-    return False
-
-def categorize_outliers(student_averages, good, regular, alert, critical):
-    outliers = []
-    
-    for name, averages in student_averages.items():
-        avg_grade = averages['average_grade']
-        avg_time = averages['average_time']
-        avg_attempts = averages['average_attempts']
-        
-        if avg_grade < 1 or avg_attempts > 4 or avg_time > 1200:
-            outliers.append([name, avg_grade, avg_time, avg_attempts, "Outlier"])
-        
-    return outliers
 
 if __name__ == "__main__":
     all_students = []
@@ -155,78 +182,113 @@ if __name__ == "__main__":
     student_attempts = group_attempts_by_student(clusters)
     student_averages = calculate_student_averages(student_attempts)
 
-    good, regular, alert, critical = categorize_students(student_averages)
-    outliers = categorize_outliers(student_averages, good, regular, alert, critical)
+    good, regular, alert, critical, list_of_recommended = categorize_students(student_averages)
 
     for student in good:
-        if not verify_student_is_outlier(student[0], outliers):
-            all_students.append(student)
+        all_students.append(student)
 
     for student in regular:
-        if not verify_student_is_outlier(student[0], outliers):
-            all_students.append(student)
+        all_students.append(student)
 
     for student in critical:
-        if not verify_student_is_outlier(student[0], outliers):
-            all_students.append(student)
+        all_students.append(student)
 
     for student in alert:
-        if not verify_student_is_outlier(student[0], outliers):
-            all_students.append(student)
+        all_students.append(student)
 
-    df_all = pd.DataFrame({
-        "Nome_do_usuario": [line[0] for line in all_students],
-        "Nota": [line[1] for line in all_students],
-        "Tempo": [line[2] for line in all_students],
-        "Tentativas": [line[3] for line in all_students],
-        "Categoria": [line[4] for line in all_students]
-    })
+    # df_all = pd.DataFrame({
+    #     "Nome_do_usuario": [line[0] for line in all_students],
+    #     "Nota": [line[1] for line in all_students],
+    #     "Tempo": [line[2] for line in all_students],
+    #     "Tentativas": [line[3] for line in all_students],
+    #     "Categoria": [line[4] for line in all_students]
+    # })
 
-    palette = {
-        'Good': 'green',
-        'Regular': 'blue',
-        'Alert': 'orange',
-        'Critical': 'red' 
-    }
+    # df_contents = pd.DataFrame({
+    #     "Nome_do_usuario": [line[0] for line in list_of_recommended],
+    #     "Email_do_usuario": [line[1] for line in list_of_recommended],
+    #     "Tentativa": [line[2] for line in list_of_recommended],
+    #     "Nota": [line[3] for line in list_of_recommended],
+    #     "Secao": [line[4] for line in list_of_recommended],
+    #     "Tipo": [line[5] for line in list_of_recommended],
+    #     "Atividade": [line[6] for line in list_of_recommended],
+    #     "Identificador_usuario": [line[6] for line in list_of_recommended]
+    # })
+
+    # palette = {
+    #     1: 'green',
+    #     2: 'blue',
+    #     3: 'orange',
+    #     4: 'red',
+    #     5: 'purple',
+    #     6: 'brown',
+    #     7: 'pink',
+    #     8: 'gray',
+    #     9: 'black',
+    #     10: 'yellow',
+    # }
 
 
-    #Relaciona as notas dos alunos com a quantidade de tentativas realizadas, separado por categoria.
-    pl.figure(figsize=(8, 6))
-    sb.scatterplot(x='Nota', y='Tentativas', data=df_all, palette=palette, hue='Categoria')
+    #  #Relaciona as notas dos alunos com a quantidade de tentativas realizadas, separado por categoria.
+    # pl.figure(figsize=(8, 6))
+    # sb.scatterplot(x='Nota', y='Tentativas', data=df_all, palette=palette, hue='Categoria')
 
-    pl.title('Relação entre Nota vs Tentativas', fontsize=16)
-    pl.xlabel('Nota', fontsize=12)
-    pl.ylabel('Tentativas', fontsize=12)
+    # pl.title('Relação entre Nota vs Tentativas', fontsize=16)
+    # pl.xlabel('Nota', fontsize=12)
+    # pl.ylabel('Tentativas', fontsize=12)
 
-    pl.savefig("Nota x Tentativas.png")
-    pl.show()
+    # pl.savefig("Nota x Tentativas.png")
+    # pl.show()
 
-    #Tentativas Realizadas versus Tempo Gasto, novamente separado por categoria. 
-    #Isso revela como o número de tentativas influencia o tempo gasto e se
-    #há padrões distintos entre as categorias
-    pl.figure(figsize=(8, 6))
-    sb.scatterplot(x='Tentativas', y='Tempo', data=df_all, palette=palette, hue='Categoria')
+    # #Visão geral do desempenho dos alunos, destacando as áreas com mais estudantes.
+    # sb.pairplot(df_all, hue='Categoria', palette=palette).savefig("Categorias_Geral.png")
+    # pl.show()
 
-    pl.title('Relação entre Tentativas vs Tempo', fontsize=16)
-    pl.xlabel('Nota', fontsize=12)
-    pl.ylabel('Tentativas', fontsize=12)
-
-    pl.savefig("Tentativas x Tempo.png")
-    pl.show()
-
-    #Visão geral do desempenho dos alunos, destacando as áreas com mais estudantes.
-    sb.pairplot(df_all, hue='Categoria', palette=palette).savefig("Categorias_Geral.png")
-    pl.show()
+    #Visão geral dos conteudos a recomendar.
+    # sb.pairplot(df_contents, hue='Secao', palette=palette).savefig("Conteudos.png")
+    # pl.show()
 
     #Relaciona as notas dos alunos com o tempo gasto nas atividades, separado por categoria. 
     #Isso ajuda a entender como a quantidade de tempo investido influencia o 
     #desempenho dos alunos em diferentes categorias.
-    pl.figure(figsize=(8, 6))
-    sb.scatterplot(x='Nota', y='Tempo', data=df_all, palette=palette, hue='Categoria')
+    # pl.figure(figsize=(8, 6))
+    # sb.scatterplot(x='Nota', y='Tempo', data=df_all, palette=palette, hue='Categoria')
 
-    pl.title('Relação entre Nota vs Tempo', fontsize=16)
-    pl.xlabel('Nota', fontsize=12)
-    pl.ylabel('Tempo (segundos)', fontsize=12)
+    # pl.title('Relação entre Nota vs Tempo', fontsize=16)
+    # pl.xlabel('Nota', fontsize=12)
+    # pl.ylabel('Tempo (segundos)', fontsize=12)
 
-    pl.savefig("Nota x Tempo.png")
-    pl.show()
+    # pl.savefig("Nota x Tempo.png")
+    # pl.show()
+
+    print(f"Alunos Bom: {len(good)}")
+    print(f"Alunos Critico: {len(critical)}")
+    print(f"Alunos Regular: {len(regular)}")
+    print(f"Alunos Alert: {len(alert)}")
+
+    total_hits = 0
+    total = len(final_good) + len(final_regular) + len(final_critical)
+
+    for student in good:
+        if verify_student_in_list(student[0], final_good):
+            total_hits += 1
+        else:
+            print(f"Good errado: {student}")
+
+    for student in regular:
+        if verify_student_in_list(student[0], final_regular):
+            total_hits += 1
+        else:
+            print(f"Regular errado: {student}")
+
+    for student in critical:
+        if verify_student_in_list(student[0], final_critical):
+            total_hits += 1
+        else:
+            print(f"Critical errado: {student}")
+
+    
+    print(f"Total de acertos: {total_hits} de {total}")
+    print(f"Porcentagem de acertos: {total_hits / (total) * 100}%")
+
+
